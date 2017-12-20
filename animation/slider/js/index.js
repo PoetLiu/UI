4ded0b7c023d6b507e528c3fcd26ab1f6696aafd
Albert Liu <457566268@qq.com>
2017-12-20 17:20:41 +0800

    Mod: Finish basic features.

--- animation/slider/js/index.js
+++ animation/slider/js/index.js
@@ -1,7 +1,49 @@
 function init() {
-    window.setInterval(function () {
-        $("#slider ul li:first-child").addClass("slide-right");
-    }, 1000);
+    var i = 0;
+    const maxId = 2;
+    const slideItem = "slider-item";
+    const slideStep = 225;
+    const slideDuration = 1000;
+    const slideInterval = 3000;
+
+    function idNext() {
+       i++;
+       if (i > maxId) {
+           i    = 0;
+           return false;
+       }
+       return true;
+    }
+
+    function slide(id) {
+        $("#"+slideItem+id).animate({
+            left: "-="+slideStep
+        }, slideDuration, function () {
+            var left = parseInt($(this).css('left'));
+            if (left === -slideStep) {
+                $(this).animate({
+                    left: "+=" + (slideStep * 2)
+                }, 0);
+            }
+        });
+    }
+
+    function getNextId() {
+        return (i === maxId) ? 0 : i+1;
+    }
+
+    function slideNext() {
+        window.setTimeout(function () {
+            var now = i, next = getNextId();
+            slide(now);
+            slide(next);
+            idNext();
+
+            slideNext();
+        }, slideInterval);
+    }
+
+    slideNext();
 }
 
 $(document).ready(function () {
