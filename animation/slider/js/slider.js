767aee61fd2e9e86f752298ee8ec55fd80c5eb8b
Albert Liu <457566268@qq.com>
2017-12-20 17:56:51 +0800

    Code optimize.

--- animation/slider/js/slider.js
+++ animation/slider/js/slider.js
@@ -13,25 +13,29 @@ Slider.prototype.idNext = function () {
     this.nowId++;
     if (this.nowId > this.itemNum) {
         this.nowId = 0;
-        return false;
     }
-    return true;
+    return this.nowId;
 };
 
 Slider.prototype.slide = function (id) {
-    var self = this;
-    $(this.itemDomId + id).animate({
-        left: "-=" + this.stepLen
-    }, this.duration, function () {
-        var left = parseInt($(this).css('left'));
-        if (left === -self.stepLen) {
+    var self = this, item = this.itemDomId+id;
+    $(item).animate(
+        {
+            left: "-=" + this.stepLen
+        },
+        this.duration,
+        function () {
             self.resetPosition(this);
         }
-    });
+    );
 };
 
-Slider.prototype.resetPosition  = function (dom) {
-    $(dom).animate({
+Slider.prototype.resetPosition  = function (item) {
+    // console.log(dom);
+    if (parseInt($(item).css('left')) !== -this.stepLen) {
+        return;
+    }
+    $(item).animate({
         left: "+=" + (this.stepLen * 2)
     }, 0);
 };
@@ -40,17 +44,17 @@ Slider.prototype.getNextId = function () {
     return (this.nowId === this.itemNum) ? 0 : this.nowId + 1;
 };
 
-Slider.prototype.slideNext = function () {
+Slider.prototype.update = function () {
     var self = this;
     window.setTimeout(function () {
         self.slide(self.nowId);
         self.slide(self.getNextId());
         self.idNext();
 
-        self.slideNext();
+        self.update();
     }, this.interval);
 };
 
 Slider.prototype.start = function () {
-    this.slideNext();
+    this.update();
 };
